eb815e8c246d0a751d1a6a3d2c6a58a7069b7495
Dregman <d.mayorchik.d@gmail.com>
2024-09-16 15:53:18 +0400

    Lesson_12_testng on TestNG

---
+++ src/FactorialCalculator.java
@@ -0,0 +1,13 @@
+public class FactorialCalculator {
+    public static long factorial(int number) {
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
